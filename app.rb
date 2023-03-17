6183ceb676c9c39e42c5bc0e036475d116d99550
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2023-03-17 15:58:19 +0100

    MVC

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader'
+require_relative 'model.rb'
 enable :sessions
 
-db = SQLite3::Database.new("db/slpws23_toiletreviews.db")
-db.results_as_hash = true
+connect_to_db()
 
 get('/')do
     slim(:login)
@@ -14,20 +14,13 @@ end
 
 post('/login') do
     username = params[:username]
-    password = params[:password]
-    result = db.execute("SELECT user_id, password_digest FROM users WHERE name = ?", username)
-    if result.empty?
+    user_id = login_user(username, params[:password])
+    if user_id == nil
       redirect('/error')
     end
-    user_id = result.first["user_id"]
-    password_digest =result.first["password_digest"]
-    if BCrypt::Password.new(password_digest) == password
-      session[:username] = username
-      session[:user_id] = user_id
-      redirect('/toilets')
-    else
-      redirect('/error')
-    end
+    session[:username] = username
+    session[:user_id] = user_id
+    redirect('/toilets')
   end
 
 post('/logout')do
@@ -40,10 +33,7 @@ get('/register')do
 end
 
 post('/register') do
-    username = params[:username]
-    password = params[:password]
-    password_digest = BCrypt::Password.create(password)
-    db.execute('INSERT INTO users (name,password_digest,role ) values (?,?,"user")', username, password_digest)
+    register_user(params[:username], params[:password])
     redirect('/')
   end
 
@@ -53,52 +43,34 @@ end
 
 get('/toilets')do
   valid_user()
-  toilets = db.execute("SELECT * FROM toilets")
-  relations = db.execute("SELECT * FROM ((attribute_toilet_relation INNER JOIN toilets ON attribute_toilet_relation.toilet_id = toilets.toilet_id) INNER JOIN attributes ON attribute_toilet_relation.attibute_id = attributes.attribute_id)")
-  toilet_attributes = Hash.new([])
-  for relation in relations
-    if !toilet_attributes.has_key?(relation["name"])
-      toilet_attributes[relation["name"]]=[]
-    end
-    toilet_attributes[relation["name"]].append(relation["type"])
-  end
+  toilets, toilet_attributes = get_toilets()
   slim(:'toilets/index', locals:{toilets:toilets, toilet_attributes:toilet_attributes})
 end
 
 post('/toilets/add') do
-  db.execute("INSERT INTO toilets (name) VALUES (?)",params[:toilet])
+  new_toilet(params[:toilet])
   redirect('/toilets')
 end
 
 get('/toilets/:id') do
   valid_user()
   id = params[:id]
-  posts = db.execute("SELECT * FROM posts INNER JOIN users ON posts.user_id = users.user_id WHERE toilet_id=?", id)
-  toilet = db.execute("SELECT name FROM toilets WHERE toilet_id=?", id).first
+  posts, toilet = get_toilet_by_id(id)
   slim(:'toilets/show', locals:{posts:posts, id:id, toilet:toilet})
 end
 
 post('/toilets/:id/add') do
-  db.execute("INSERT INTO posts (text, rating, toilet_id, user_id) VALUES (?, ?, ?, ?)",params[:text], params[:rating].to_i, params[:id], session[:user_id])
-  redirect("/toilets/#{params[:id]}")
+  id = params[:id]
+  new_toilet(params[:text], params[:rating].to_i, id, session[:user_id])
+  redirect("/toilets/#{id}")
 end
 
 post('/toilets/:id/:post_id/update')do
-  if params[:new_text] != ""
-    db.execute("UPDATE posts SET text = ?, rating = ? WHERE post_id = ?",params[:new_text], params[:new_rating].to_i, params[:post_id])
-  else
-    db.execute("UPDATE posts SET rating = ? WHERE post_id = ?", params[:new_rating].to_i, params[:post_id])
-  end
+  update_post(params[:new_text], params[:new_rating].to_i, params[:post_id])
   redirect("/toilets/#{params[:id]}")
 end
 
 post('/toilets/:id/:post_id/delete')do
-  db.execute("DELETE FROM posts WHERE post_id = ?", params[:post_id])
+  delete_post(params[:post_id])
   redirect("/toilets/#{params[:id]}")
-end
-
-def valid_user()
-  if session[:user_id] == nil
-    redirect('/')
-  end
 end
---
+++ model.rb
@@ -0,0 +1,67 @@
+def valid_user()
+    if session[:user_id] == nil
+      redirect('/')
+    end
+end
+
+def connect_to_db()
+    db = SQLite3::Database.new("db/slpws23_toiletreviews.db")
+    db.results_as_hash = true
+end
+
+def register_user(username, password)
+    password_digest = BCrypt::Password.create(password)
+    db.execute('INSERT INTO users (name,password_digest,role ) values (?,?,"user")', username, password_digest)
+end
+
+def login_user(username, password)
+    result = db.execute("SELECT user_id, password_digest FROM users WHERE name = ?", username)
+    if result.empty?
+      return false
+    end
+    user_id = result.first["user_id"]
+    password_digest = result.first["password_digest"]
+    if BCrypt::Password.new(password_digest) != password
+        return false
+    end
+    return user_id
+end
+
+def get_toilets()
+    toilets = db.execute("SELECT * FROM toilets")
+    relations = db.execute("SELECT * FROM ((attribute_toilet_relation INNER JOIN toilets ON attribute_toilet_relation.toilet_id = toilets.toilet_id) INNER JOIN attributes ON attribute_toilet_relation.attibute_id = attributes.attribute_id)")
+    toilet_attributes = Hash.new([])
+    for relation in relations
+        if !toilet_attributes.has_key?(relation["name"])
+            toilet_attributes[relation["name"]]=[]
+        end
+        toilet_attributes[relation["name"]].append(relation["type"])
+    end
+    return [toilets, toilet_attributes]
+end
+
+def new_toilet(new_toilet)
+    db.execute("INSERT INTO toilets (name) VALUES (?)",new_toilet)
+end
+
+def get_toilet_by_id(id)
+    posts = db.execute("SELECT * FROM posts INNER JOIN users ON posts.user_id = users.user_id WHERE toilet_id=?", id)
+    toilet = db.execute("SELECT name FROM toilets WHERE toilet_id=?", id).first
+    return[posts, toilet]
+end
+
+def new_review(text, rating, id, user_id)
+    db.execute("INSERT INTO posts (text, rating, toilet_id, user_id) VALUES (?, ?, ?, ?)", text, rating, id, user_id)
+end
+
+def delete_post(post_id)
+    db.execute("DELETE FROM posts WHERE post_id = ?", post_id)
+end
+
+def update_post(new_text, new_rating, post_id)
+    if params[:new_text] != ""
+        db.execute("UPDATE posts SET text = ?, rating = ? WHERE post_id = ?",new_text, new_rating, post_id)
+      else
+        db.execute("UPDATE posts SET rating = ? WHERE post_id = ?", new_rating, post_id)
+      end
+end
